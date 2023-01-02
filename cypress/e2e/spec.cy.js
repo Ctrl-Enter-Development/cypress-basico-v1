6e1f03ff0d15b9d6937f0d1f8d0d5072f1974fb5
Clintondev <clintonrodrigues.dev@gmail.com>
2023-01-02 16:01:51 -0300

    fix cypress v9

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -152,5 +152,15 @@ it('preenche os campos obrigatórios e envia o formulário com o clock', functio
   cy.get('.success').should('not.be.visible')
 })
 
+it('acessa a página da política de privacidade removendo o target e então clicando no link',function(){
+  cy.get('#privacy a').invoke('removeAttr','target').click()
+  cy.contains('Talking About Testing').should('be.visible')
+})
+
+it('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique',function(){
+  cy.get('#privacy a').should('have.attr','target','_blank')
+})
+
+
 
 })
